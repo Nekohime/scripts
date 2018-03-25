3771b36caf20f2bb2dfbc8dc627bbb85787a1556
Nekohime <nekohime@users.noreply.github.com>
2018-03-25 17:59:55 -0400

    Added hidethiswindow.sh - Hides focused window. Useful for hiding the runescape splash screen mostly.

---
+++ hidethiswindow.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#Makes your docused window hidden
+#Only use I found for this (which was also a suggested solution) was to hide the RuneScape NXT splash screen (NXT 2.2.4 at the time of writing this has a bug where
+#the splash screen never goes away. It's been almost a year...)
+sleep 2
+wid=`xdotool getactivewindow` 
+xdotool windowunmap $wid

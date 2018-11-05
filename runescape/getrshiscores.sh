d0c224cc473f7c87e7874e3dd646b6d427fac059
Nekohime <Nekohime@users.noreply.github.com>
2018-11-05 16:03:27 -0500

    Complying with ShellCheck's demands Deleted wfind as I found no real need for it? And it was broken anyway.

--- runescape/getrshiscores.sh
+++ runescape/getrshiscores.sh
@@ -15,9 +15,9 @@ hiscores_json=$playerName.json # Hiscores JSON filename
 
 #if [ ! -f ./$hiscores_json ]; then # We don't actually need this... if would actually prevent us from updating. Could fix by making an --update flag. Not rn though.
 
-  wget -q $url -O ./$hiscores_csv.tmp # Download the Hiscore for $playerName into a temporary file
-  sed '29,$d' $hiscores_csv.tmp > $hiscores_csv # Cut the minigame bs out of the temporary csv file
-  rm $hiscores_csv.tmp # Delte the temporary file as we're done with it
+  wget -q "$url" -O "./$hiscores_csv.tmp" # Download the Hiscore for $playerName into a temporary file
+  sed '29,$d' "$hiscores_csv.tmp" > "$hiscores_csv" # Cut the minigame bs out of the temporary csv file
+  rm "$hiscores_csv.tmp" # Delte the temporary file as we're done with it
 
 # The List of RuneScape Skills. Their names lowercased. 26 of them + overall/total stuff
 declare -a skillsList=(
@@ -27,13 +27,14 @@ declare -a skillsList=(
 "hunter" "construction" "summoning" "dungeoneering" "divination" "invention")
 
 #We read the csv file into an array called skillsCSV
-readarray csvLines < $hiscores_csv
+readarray csvLines < "$hiscores_csv"
 
 
 jsonOutput+="{" # Starting JSON...
 
 for ((i=0;i<${#csvLines[@]};++i)); do # Black Magic
-  IFS=',' read -a splitValues <<< ${csvLines[$i]}
+  #IFS=',' read -a splitValues <<< ${csvLines[$i]}
+  IFS=',' read -ra splitValues <<< "${csvLines[$i]}"
   if [ "$i" -ne 27 ]; then
     jsonOutput+="\"${skillsList[$i]}\": { \"rank\": ${splitValues[0]}, \"level\": ${splitValues[1]}, \"experience\": ${splitValues[2]} },"
   else
@@ -43,6 +44,6 @@ done
 
 jsonOutput+="}" # Ending JSON...
 #echo $jsonOutput #This is here for displaying in terminal when debugging the script
-echo $jsonOutput > $hiscores_json
-rm $hiscores_csv
+echo "$jsonOutput" > "$hiscores_json"
+rm "$hiscores_csv"
 #fi # end of this if, earlier in the script: if [ ! -f ./$hiscores_json ]; then

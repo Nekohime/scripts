f834efbb4791e84af298116069f05204905efb23
Nekohime <Nekohime@users.noreply.github.com>
2018-12-23 10:25:15 -0500

    Added a fix for firefox Added clarification to the kwin.sh file

---
+++ fixes/force-portals.sh
@@ -0,0 +1,5 @@
+#!/etc/profile.d/force-portals.sh
+# Useful for making Firefox make a bit more sense.
+# (Save/Open KDE Dialog boxes, instead of gnome's nonsense)
+
+export GTK_USE_PORTAL=1
--- fixes/kwin.sh
+++ fixes/kwin.sh
@@ -1,2 +1,4 @@
 #/etc/profile.d/kwin.sh
+# Helps with nvidia's best feature: screen tearing
+
 export KWIN_TRIPLE_BUFFER=1

8db51d4069b5286a81f0aaafb681dfd608af1044
Nekohime <Nekohime@users.noreply.github.com>
2018-12-23 10:27:37 -0500

    Oops

--- fixes/force-portals.sh
+++ fixes/force-portals.sh
@@ -1,4 +1,4 @@
-#!/etc/profile.d/force-portals.sh
+#/etc/profile.d/force-portals.sh
 # Useful for making Firefox make a bit more sense.
 # (Save/Open KDE Dialog boxes, instead of gnome's nonsense)
 
--- fixes/kwin.sh
+++ fixes/kwin.sh
@@ -1,4 +1,4 @@
 #/etc/profile.d/kwin.sh
-# Helps with nvidia's best feature: screen tearing
+# Helps with nvidia's best feature: screen tearing (You should be forcing full composition pipeline too)
 
 export KWIN_TRIPLE_BUFFER=1

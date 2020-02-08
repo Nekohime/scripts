74dcef9461e2110292bfe945e1de4e150ccbab2e
Nekohime <Nekohime@users.noreply.github.com>
2020-02-07 23:06:05 -0500

    Added todo.sh, a simple todo list manager Added some youtube-dl/music related scripts Fixed zip_all_individually

---
+++ utils/todo.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# Released under public domain. Made my Nekohime (https://nekohime.net)
+# I hope it will be useful!
+# Fun fact: Written in about an hour
+
+# You should put some exports in your .bashrc, but this will handle defaults as well.
+#export TODO_FILE=~/todo/todo.txt
+
+TODO_FOLDER=~/todo/
+TODO_FILE=$TODO_FOLDER/list.txt
+mkdir -p $TODO_FOLDER
+
+# Didn't use a switch case to make the code more readable for myself.
+# Sorry, not sorry :)
+
+if [ "$1" == "list" ] || [ "$1" == "l" ]; then
+	grep -rn '' $TODO_FILE --color=always
+elif [ "$1" == "search" ] || [ "$1" == "s" ]; then
+	grep -rni "$2" $TODO_FILE --color=always
+elif [ "$1" == "add" ] || [ "$1" == "a" ]; then
+	echo "$2" >> $TODO_FILE
+elif [ "$1" == "remove" ] || [ "$1" == "r" ]; then
+	sed -i "$2"d $TODO_FILE #grep -rn '' $TODO_FILE --color=always
+elif [ "$1" == "edit" ] || [ "$1" == "e" ]; then
+	echo "NYI!"
+else
+	grep -rn '' $TODO_FILE --color=always # Default to list
+fi

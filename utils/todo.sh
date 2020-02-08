581c68a2dc7494d622b57c7982765c4a30aa40ac
Nekohime <Nekohime@users.noreply.github.com>
2020-02-08 12:32:36 -0500

    Added displaying the todo file in use, and checks to make sure it only gets displayed when we find something

--- utils/todo.sh
+++ utils/todo.sh
@@ -6,6 +6,8 @@
 # You should put some exports/alias(es) in your .bashrc, but this will handle defaults as well.
 #export TODO_FILE=~/.todo/list.txt
 #alias todo='bash ~/Documents/git/BashScripts/utils/todo.sh'
+# Could also use like this: TODO_FILE=~/.todo/list.txt todo <commands>
+# Could also make a new alias that uses a different todo file :)
 
 if [ -z "$TODO_FILE" ]; then
  #echo "TODO_FILE is Empty... using default"
@@ -17,10 +19,23 @@ touch $TODO_FILE
 
 # Didn't use a switch case to make the code more readable for myself.
 # Sorry, not sorry :)
+
+function displayFile() {
+	if [ $(stat -c %s $TODO_FILE) -gt 0 ]; then
+		echo $TODO_FILE
+	fi
+}
+
 if [ "$1" == "list" ] || [ "$1" == "l" ]; then #todo list
-	grep -rn '' $TODO_FILE --color=always
+	if [ -n "$(grep -rn '' $TODO_FILE --color=always)" ]; then
+    displayFile
+		grep -rn '' $TODO_FILE --color=always
+	fi
 elif [ "$1" == "search" ] || [ "$1" == "s" ]; then #todo search "string"
-	grep -rni "$2" $TODO_FILE --color=always
+  if [ -n "$(grep -rni "$2" $TODO_FILE --color=always)" ]; then
+    displayFile
+		grep -rni "$2" $TODO_FILE --color=always
+	fi
 elif [ "$1" == "add" ] || [ "$1" == "a" ]; then #todo add "string"
 	echo "$2" >> $TODO_FILE
 elif [ "$1" == "remove" ] || [ "$1" == "r" ]; then #todo remove <line number>
@@ -28,5 +43,6 @@ elif [ "$1" == "remove" ] || [ "$1" == "r" ]; then #todo remove <line number>
 elif [ "$1" == "edit" ] || [ "$1" == "e" ]; then #todo edit <line number> "string"
 	sed -i "${2}s/.*/$3/" $TODO_FILE
 else
+	displayFile
 	grep -rn '' $TODO_FILE --color=always # Default to list
 fi

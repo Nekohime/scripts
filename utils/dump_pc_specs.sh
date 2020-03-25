638956c2551534b4cc8feba78d1041211ac524ef
Nekohime <Nekohime@users.noreply.github.com>
2020-03-25 15:21:24 -0400

    Added an emulator launcher to the retrostation project Added a GB/GBC/GBA game launcher to the retrostation project utils/dump_pc_specs.sh made to give more info, I think?

--- utils/dump_pc_specs.sh
+++ utils/dump_pc_specs.sh
@@ -4,8 +4,11 @@ rm specs.tar.gz
 
 lspci > lspci.log
 lsusb > lsusb.log
-inxi -F > inxi.log
+#inxi -F > inxi.log
+sudo inxi -FmB -v 7 > inxi.log
+sudo chown $USER:$USER inxi.log
 
 tar -cvf specs.tar.gz *.log
 
 rm *.log
+
